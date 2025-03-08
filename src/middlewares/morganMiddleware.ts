1c1b3647a091b1166b74c42ed0b18634ea240cbb
pilyeooong <pilyeooong@gmail.com>
2025-03-08 13:09:01 +0900

    add. comment

--- src/middlewares/morganMiddleware.ts
+++ src/middlewares/morganMiddleware.ts
@@ -7,12 +7,12 @@ morgan.token('transactionId', (req: Request) => req.transactionId || randomUUID(
 
 // morgan에서 제공하는 format 가이드
 // https://expressjs.com/en/resources/middleware/morgan.html
-
 const format =
   process.env.NODE_ENV === 'production'
     ? ':transactionId :remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :response-time ms :res[content-length] ":referrer" ":user-agent"'
     : ':transactionId :method :url :status :response-time ms - :res[content-length]';
 
+// 모든 http 요청을 로깅 미들웨어로 스트림 합니다.
 const morganMiddleware = morgan(format, {
   stream: {
     write: (message) => logger.http(message),
--- src/middlewares/transactionIdMiddleware.ts
+++ src/middlewares/transactionIdMiddleware.ts
@@ -1,6 +1,7 @@
 import { randomUUID } from 'crypto';
 import { NextFunction, Request, Response } from 'express';
 
+// 어플리케이션 서버로의 모든 요청마다 고유한 id 값을 부여합니다.
 const transactionIdMiddleware = (req: Request, res: Response, next: NextFunction) => {
   req.transactionId = randomUUID();
   res.locals.transactionId = req.transactionId;
