d4272042193fbfda6359808159bdf6431f5a9c6a
pilyeooong <pilyeooong@gmail.com>
2025-03-08 10:24:46 +0900

    add. setup docker, database

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,18 @@
 import express from 'express';
 import router from 'routes';
+import sequelize from 'database';
 
 const createApp = async () => {
   const app = express();
 
   app.use(router);
+
+  try {
+    await sequelize.authenticate();
+    console.log('Database successfully connected.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
 
   return app;
 };
