f9f545f786473c55007974ab92c212475a6e0839
pilyeooong <pilyeooong@gmail.com>
2025-03-15 16:35:15 +0900

    fix. redis host

--- src/test/setEnvVars.ts
+++ src/test/setEnvVars.ts
@@ -3,5 +3,5 @@ process.env.DATABASE_NAME = 'wavedeck-test';
 process.env.DATABASE_PORT = '3307';
 process.env.PORT = '3000';
 process.env.NODE_ENV = 'test';
-process.env.REDIS_HOST = 'localhost';
+process.env.REDIS_HOST = process.env.CI ? 'redis' : 'localhost';
 process.env.REDIS_PORT = '6379';
